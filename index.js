505a4b0fd9eaeae66cf063e2e9729db914ad0b1e
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-24 09:25:24 +0300

    Uses control flows in Javascript to set conditions with specific and expected return values

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let response = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let response;
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+
+    case "not as generous":
+      response = "Thank you."
+      break;
+
+    default:
+      response = "Bye."
+  }
+  return `${response}`;
 }
